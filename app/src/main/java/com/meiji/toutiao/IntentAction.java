2fa9c7fe5c14ef9a335f86014e4eab5cfa5fc81b
iMeiji <iMeiji@users.noreply.github.com>
2018-03-24 11:32:57 +0800

    Add Image Share

--- app/src/main/java/com/meiji/toutiao/IntentAction.java
+++ app/src/main/java/com/meiji/toutiao/IntentAction.java
@@ -2,6 +2,7 @@ package com.meiji.toutiao;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 
 /**
@@ -15,6 +16,14 @@ public class IntentAction {
                 .setAction(Intent.ACTION_SEND)
                 .setType("text/plain")
                 .putExtra(Intent.EXTRA_TEXT, shareText);
+        context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share_to)));
+    }
+
+    public static void sendImage(@NonNull Context context, @NonNull Uri uri) {
+        Intent shareIntent = new Intent()
+                .setAction(Intent.ACTION_SEND)
+                .setType("image/*")
+                .putExtra(Intent.EXTRA_STREAM, uri);
         context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share_to)));
     }
 }
